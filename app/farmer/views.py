ed56647e411d3dfd7be2fe7c8f68f7d69c29139f
timurkulbachaev03 <kulbachaevtimur@gmail.com>
2023-02-22 14:15:17 +0600

    test3

--- app/farmer/views.py
+++ app/farmer/views.py
@@ -33,21 +33,20 @@ class OrderViewSet(viewsets.ModelViewSet):
             return serializers.GetOrderSerializer
         return serializers.OrderSerializer
 
-    # def update(self, request, *args, **kwargs):
-    #     partial = kwargs.pop('partial', False)
-    #     instance = models.FarmerOrders.objects.get(pk=request.data['id'])
-    #     req_items = request.data['items']
-    #     for i in req_items:
-    #         item = models.CartItems.objects.create(item=i['item'],
-    #                                                quantity=i['quantity'])
-    #     instance.items = item
-    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_update(serializer)
-    #
-    #     if getattr(instance, '_prefetched_objects_cache', None):
-    #         # If 'prefetch_related' has been applied to a queryset, we need to
-    #         # forcibly invalidate the prefetch cache on the instance.
-    #         instance._prefetched_objects_cache = {}
-    #
-    #     return Response(serializer.data)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        req_items = request.data['items']
+        for i in req_items:
+            item = models.CartItems.objects.create(item=i['item'],
+                                                   quantity=i['quantity'])
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
