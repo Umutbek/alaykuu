2d8497db0b04ab6e3988812217092395e79332ff
timurkulbachaev03 <kulbachaevtimur@gmail.com>
2023-02-27 15:33:25 +0600

    test

--- app/app/settings.py
+++ app/app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-rzp+()6osj**nhxtvw!pcp01$vk5ahcu7&e+qme(jpoliss10x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
