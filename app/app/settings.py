235cc3932dc922ddbbed523ce6eeb5215d8a7028
timurkulbachaev03 <kulbachaevtimur@gmail.com>
2023-03-29 12:13:24 +0600

    jwt settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,8 +127,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
