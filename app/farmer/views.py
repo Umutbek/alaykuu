1cde2075dc7a110c27430c57d2d19e4a002f9fc9
timurkulbachaev03 <kulbachaevtimur@gmail.com>
2023-04-01 15:54:25 +0600

    order update ver19

--- app/farmer/views.py
+++ app/farmer/views.py
@@ -36,6 +36,21 @@ class OrderViewSet(viewsets.ModelViewSet):
             return serializers.OrderSerializerUpdate
         return serializers.OrderSerializer
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+
+        return Response(serializer.data)
+
 
 class CartItemViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
