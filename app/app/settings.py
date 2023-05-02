9a660959ac7a080ab8928af5875a1d091670d4cb
timurkulbachaev03 <kulbachaevtimur@gmail.com>
2023-05-02 14:09:07 +0600

    csrf token

--- app/app/settings.py
+++ app/app/settings.py
@@ -318,7 +318,7 @@ AUTH_USER_MODEL = 'user.User'
 
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
-# CSRF_TRUSTED_ORIGINS = ['http://176.126.164.39', 'http://alaiku.com/', 'https://alaiku.com/']
+CSRF_TRUSTED_ORIGINS = ['http://176.126.164.39', 'http://alaiku.com/', 'https://alaiku.com/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
