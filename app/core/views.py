50257b00f70e632ae977f4562d36d99fd9ac1562
Timur <kulbachaevtimur@gmail.com>
2023-06-22 12:36:04 +0600

    test9

--- app/core/views.py
+++ app/core/views.py
@@ -263,11 +263,6 @@ class SyncWithOneCViewSet(APIView):
                     ]
                 }
 
-                # json_object_without_bom = remove_bom_from_json(send_data)
-                #
-                # oneC_request = requests.post('http://212.42.107.229/alayku/hs/exchange/document/purchase/',
-                #                              json=json_object_without_bom, headers=headers)
-                # # request_response = remove_bom(oneC_request.json())
                 send_data_json = json.dumps(send_data)  # Преобразование словаря send_data в JSON строку
                 send_data_json_bytes = send_data_json.encode('utf-8')  # Кодирование JSON строки в байты
 
@@ -279,6 +274,8 @@ class SyncWithOneCViewSet(APIView):
                 oneC_request = requests.post('http://212.42.107.229/alayku/hs/exchange/document/purchase/',
                                              json=send_data_without_bom, headers=headers)
                 response_data.append(oneC_request.json())
+                ref = oneC_request.json()['Ref']
+                accepted_product.ref = ref
             else:
                 message = {"message": f"The ref model field of the accepted product with this ID exists ({i['id']})"}
                 response_data.append(message)
