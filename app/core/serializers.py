23f6d59197ea11c7c94921eeaa087802970a86b2
Kulbachaev Timur <kulbachaevtimur@gmail.com>
2023-08-26 19:59:34 +0600

    add new fields to accepted product model (date_second)

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -40,7 +40,7 @@ class GetAcceptedSerializer(serializers.ModelSerializer):
         model = models.Accepted
         fields = (
             'id', 'item', 'farmer', 'distributor', 'amount', 'unit', 'unitCost', 'discount', 'totalCost', 'status',
-            'comment', 'sort', 'fat', 'acidity', 'date', 'payment_method', 'sync_with_oneC', 'ref',
+            'comment', 'sort', 'fat', 'acidity', 'date', 'date_second', 'payment_method', 'sync_with_oneC', 'ref',
             'farmerReview', 'farmerComment'
             )
 
@@ -54,7 +54,7 @@ class AcceptedSerializer(serializers.ModelSerializer):
         model = models.Accepted
         fields = (
             'id', 'item', 'farmer', 'distributor', 'amount', 'unit', 'unitCost', 'discount', 'totalCost', 'status',
-            'comment', 'sort', 'fat', 'acidity', 'date', 'payment_method', 'ref', 'farmerReview', 'farmerComment'
+            'comment', 'sort', 'fat', 'acidity', 'date', 'date_second', 'payment_method', 'ref', 'farmerReview', 'farmerComment'
             )
 
         read_only_fields = ('id', 'date')
