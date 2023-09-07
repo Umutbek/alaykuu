de05b9150c5fc2344c4785694a66b953b22cebf5
Kulbachaev Timur <kulbachaevtimur@gmail.com>
2023-09-07 12:45:57 +0600

    change password api test 8

--- app/app/settings.py
+++ app/app/settings.py
@@ -327,7 +327,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'alaiku.apps@gmail.com'
-EMAIL_HOST_PASSWORD = 'sbvjkyvtяlяumwяq'
+EMAIL_HOST_PASSWORD = 'bdrzalryekbuvkhd'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
